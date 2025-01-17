d5973f982ac57c70011b99adac160e4bc4e78d68
Adrian Vicente <>
2025-01-17 09:50:15 +0100

    primeros cambios
    añadida carpeta js y creado archivo 02-variables, movido el index a raiz de carpeta

---
+++ Curso Git/js/02-variables.js
@@ -0,0 +1,25 @@
+
+//VARIABLES
+//Una variable es un contenedor de informacion
+
+//Variables de tipo string 
+var pais="España";
+var continente="Europa";
+
+console.log(pais);
+console.log(pais, continente);
+
+pais="Mexico";
+continente="Latinoamerica";
+
+console.log(pais);
+console.log(pais, continente);
+
+//Variables de tipo entero
+var antiguedad= 2019;
+console.log(antiguedad);
+console.log(pais, continente, antiguedad);
+
+//Concatenar variables
+var pais_continente=pais+' '+continente;
+alert(pais_continente);
