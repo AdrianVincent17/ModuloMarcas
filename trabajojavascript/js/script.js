d4ba3d9dd7a285fe68b6bee6c0b0c361646fca49
AdrianVincent17 <adrian.vicente.lopez@ces-vegamedia.es>
2025-02-20 20:46:03 +0100

    añadido el boton de reiniciar

--- trabajojavascript/js/script.js
+++ trabajojavascript/js/script.js
@@ -92,6 +92,31 @@ function turnoMaquina()
     finDelJuego();
 }
 
+function botonreset(){
+
+    resultUsuario = 0;  //variable que contiene como contador el resultado del usuario
+    resultMaquina = 0;  //variable que contiene como contador el resultado de la maquina
+    document.getElementById("resultusuarito").innerHTML = "";
+    document.getElementById("resultmaquinita").innerHTML = "";
+    document.getElementById("mensaje").innerHTML = "";
+    document.getElementById("cartastapete").innerHTML="";
+    document.getElementById("pedidico").disabled = false;
+    document.getElementById("plantadico").disabled=false;
+    var audioPierdes = document.getElementById("pierdes");
+    audioPierdes.pause();
+    audioPierdes.currentTime=0;
+    var audioGanas = document.getElementById("ganas");
+    audioGanas.pause();
+    audioGanas.currentTime=0;
+
+    var arrayImg=["N1.png","N2.png","N3.png","N4.png","N5.png","N6.png","N7.png","NS.png","NP.png","NZ.png",
+        "A1.png","A2.png","A3.png","A4.png","A5.png","A6.png","A7.png","AS.png","AP.png","AZ.png",
+        "L1.png","L2.png","L3.png","L4.png","L5.png","L6.png","L7.png","LS.png","LP.png","LZ.png",
+        "V1.png","V2.png","V3.png","V4.png","V5.png","V6.png","V7.png","VS.png","VP.png","VZ.png"];
+    var arrayValor=[1,2,3,4,5,6,7,0.5,0.5,0.5,1,2,3,4,5,6,7,0.5,0.5,0.5,1,2,3,4,5,6,7,0.5,0.5,0.5,1,2,3,4,5,6,7,0.5,0.5,0.5];
+
+}
+
 
 function finDelJuego() {
     
@@ -120,7 +145,7 @@ function finDelJuego() {
         audioPierdes.play();
      
     } else {
-        mensaje = "La máquinita te ha ganado.";
+        mensaje = "La maquinita te ha ganado.";
 
         //y aqui tambien reproducimos la musica
         var audioPierdes = document.getElementById("pierdes");
